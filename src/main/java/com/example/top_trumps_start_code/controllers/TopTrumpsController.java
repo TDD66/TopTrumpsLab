46455fb6aecba9d7698c969b09691cbc25b731f1
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-02-26 15:02:53 +0000

    Change name of path.

--- src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
+++ src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
@@ -2,6 +2,9 @@ package com.example.top_trumps_start_code.controllers;
 
 import com.example.top_trumps_start_code.models.Card;
 import com.example.top_trumps_start_code.models.Reply;
+import com.example.top_trumps_start_code.services.TopTrumpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,14 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping(value = "/games")
+@RequestMapping(value = "/topTrumps")
 public class TopTrumpsController {
 
+    @Autowired
+    TopTrumpService topTrumpService;
 
     @PostMapping
     public ResponseEntity<Reply> createGame(@RequestBody ArrayList<Card> match){
         // Create game
         // reply = topTrumpsService.checkWinner(match);
         // return new ResponseEntity<>(reply, HttpStatus.OK);
+        Reply reply = topTrumpService.checkWinner(match);
+        return new ResponseEntity<>(reply, HttpStatus.CREATED);
     }
 }
